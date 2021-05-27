1375c5ebbae304e666bb8809a3ca4b68c5fff3ed
Vadavur <vadavur@gmail.com>
2021-05-27 13:57:30 +0300

    refactor: change HeaderField class logic, so it would append whole header control panel instead of just a button

--- src/components/header-field/header-field.ts
+++ src/components/header-field/header-field.ts
@@ -1,29 +1,17 @@
 import './header-field.scss';
 import { BaseComponent } from '../shared/base-component';
-import { PopupField } from '../popup-field/popup-field';
 import { NavField } from '../nav-field/nav-field';
-import { Button } from '../button/button';
+import { HeaderControlPanel } from '../header-control-panel/header-control-panel';
 
 export class HeaderField extends BaseComponent {
   private readonly navField: NavField;
-
-  private readonly headerButton: Button;
 
   MAIN_LOGO_URL = 'src/assets/images/match-match-logo.png';
 
   constructor() {
     super('div', ['header-field']);
 
-    const showPopup = (): void => {
-      document.body.appendChild(new PopupField().element);
-    };
-
     this.navField = new NavField();
-    this.headerButton = new Button(
-      ['button_register-new-player'],
-      'register new player',
-      showPopup
-    );
 
     this.element.innerHTML = `
       <img
@@ -34,6 +22,7 @@ export class HeaderField extends BaseComponent {
     `;
 
     this.element.appendChild(this.navField.element);
-    this.element.appendChild(this.headerButton.element);
+    this.element.appendChild(HeaderControlPanel.controlElement);
+    HeaderControlPanel.toggleControlPanel();
   }
 }
